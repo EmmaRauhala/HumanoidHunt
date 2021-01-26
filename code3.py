fe597c77aeac88bb3b82049408878f1c210428d4
Emma Rauhala <emma.rauhala@aalto.fi>
2021-01-27 00:50:31 +0200

    code3 with some comments

--- code3.py
+++ code3.py
@@ -1,116 +1,118 @@
 import numpy as np
 import csv
 
+# read data (saved as csv)
 data = open('data3.csv','r')
 data = csv.reader(data, delimiter=',', quotechar='|')
 
+
 matrix = np.ones((200,200))
+startpoint = tuple()
 endpoints = []
 
-# whatif changes
-
+# go through all the neural strands constructing a binary matrix representing possible routes
+# please note that here zeros represent routes/connections and ones represent "walls" 
 for line in data:
+
+    # starting point
     x = int(line[0])
-    print(x)
     y = int(line[1])
-    print(y)
     matrix[x][y] = 0
+
+    # following the directions
     for char in line[2:]:
         if char == 'D':
             y = y+1
-        if char == 'U':
+        elif char == 'U':
             y = y-1
-        if char == 'R':
+        elif char == 'R':
             x = x+1
-        if char == 'L':
+        elif char == 'L':
             x = x-1
-        if char == 'F':
+        # listing also the endpoints
+        elif char == 'F':
             endpoints.append((x,y))
+        # and finding the starting point
+        elif char == 'S':
+            startpoint = (x,y)
+        # adding resulting routes to the matrix    
         matrix[x][y] = 0
-print(matrix)
-print(endpoints)
+#print(matrix)
+#print(endpoints)
+#print(startpoint)
 
-m = np.zeros((200,200))
+routematrix = np.zeros((200,200))
 
 def make_step(k):
-  for i in range(len(m)):
-    for j in range(len(m[i])):
-      if m[i][j] == k:
-        if i>0 and m[i-1][j] == 0 and matrix[i-1][j] == 0:
-          m[i-1][j] = k + 1
-        if j>0 and m[i][j-1] == 0 and matrix[i][j-1] == 0:
-          m[i][j-1] = k + 1
-        if i<len(m)-1 and m[i+1][j] == 0 and matrix[i+1][j] == 0:
-          m[i+1][j] = k + 1
-        if j<len(m[i])-1 and m[i][j+1] == 0 and matrix[i][j+1] == 0:
-           m[i][j+1] = k + 1
+  for i in range(len(routematrix)):
+    for j in range(len(routematrix[i])):
+      if routematrix[i][j] == k:
+        if i>0 and routematrix[i-1][j] == 0 and matrix[i-1][j] == 0:
+          routematrix[i-1][j] = k + 1
+        if j>0 and routematrix[i][j-1] == 0 and matrix[i][j-1] == 0:
+          routematrix[i][j-1] = k + 1
+        if i<len(routematrix)-1 and routematrix[i+1][j] == 0 and matrix[i+1][j] == 0:
+          routematrix[i+1][j] = k + 1
+        if j<len(routematrix[i])-1 and routematrix[i][j+1] == 0 and matrix[i][j+1] == 0:
+           routematrix[i][j+1] = k + 1
 
-i,j = 2,2
-m[i][j] = 1
+# initialize the starting point of the route
+routematrix[startpoint[0]][startpoint[1]] = 1
 
 k = 0
-while all([m[end[0]][end[1]] == 0 for end in endpoints]):
+while all([routematrix[end[0]][end[1]] == 0 for end in endpoints]):
     k += 1
-    print(k)
     make_step(k)
 
-print(all([m[end[0]][end[1]] == 0 for end in endpoints]))
+print(all([routematrix[end[0]][end[1]] == 0 for end in endpoints]))
 
-#find endpoint
+# find the endpoint we ended up in
 endpoint = (0,0)
 for end in endpoints:
-    if m[end[0]][end[1]] != 0:
+    if routematrix[end[0]][end[1]] != 0:
        endpoint = (end[0],end[1])
        print(endpoint)
 
 i, j = endpoint
-k = m[i][j]
-the_path = [(i,j)]
+k = routematrix[i][j]
+path = [(i,j)]
 while k > 1:
-  if i > 0 and m[i - 1][j] == k-1:
+  if i > 0 and routematrix[i - 1][j] == k-1:
     i, j = i-1, j
-    the_path.append((i, j))
+    path.append((i, j))
     k-=1
-  elif j > 0 and m[i][j - 1] == k-1:
+  elif j > 0 and routematrix[i][j - 1] == k-1:
     i, j = i, j-1
-    the_path.append((i, j))
+    path.append((i, j))
     k-=1
-  elif i < len(m) - 1 and m[i + 1][j] == k-1:
+  elif i < len(routematrix) - 1 and routematrix[i + 1][j] == k-1:
     i, j = i+1, j
-    the_path.append((i, j))
+    path.append((i, j))
     k-=1
-  elif j < len(m[i]) - 1 and m[i][j + 1] == k-1:
+  elif j < len(routematrix[i]) - 1 and routematrix[i][j + 1] == k-1:
     i, j = i, j+1
-    the_path.append((i, j))
+    path.append((i, j))
     k -= 1
 
 
 
-the_path.reverse()
-
-path = the_path
+path.reverse()
 
 print(path)
 
-instructions = []
+instructions = ""
 
 curr = path[0]
 
 for cell in path:
     if cell[0] > curr[0]:
-        instructions.append('R')
+        instructions = instructions + 'R'
     if cell[0] < curr[0]:
-        instructions.append('L')
+        instructions = instructions + 'L'
     if cell[1] > curr[1]:
-        instructions.append('D')
+        instructions = instructions + 'D'
     if cell[1] < curr[1]:
-        instructions.append('U')
+        instructions = instructions + 'U'
     curr = cell
 
 print(instructions)
-print(''.join(instructions))
-text_file = open("Output.txt", "w")
-
-text_file.write(''.join(instructions))
-
-text_file.close()
